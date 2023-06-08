554286abeae94c5dc251d86a62a630257036d8ca
Raj-55 <113324198+Raj-55@users.noreply.github.com>
2023-06-08 22:58:17 +0530

    Update xor_test.py

--- tests/xor_test.py
+++ tests/xor_test.py
@@ -7,8 +7,8 @@ async def xor_test(dut):
    a = (0, 0, 1, 1)
    b = (0, 1, 0, 1)
    y = (0, 1, 1, 0)
-
-    for i in range(4):
+   
+   for i in range(4):
         dut.a.value = a[i]
         dut.b.value = b[i]
         await Timer(1, 'ns')
