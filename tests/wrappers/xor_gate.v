95644ee21654f19edabe6a590fdcfcdd31cd62ec
Raj-55 <113324198+Raj-55@users.noreply.github.com>
2023-06-08 14:41:08 +0530

    Update xor_gate.v

--- tests/wrappers/xor_gate.v
+++ tests/wrappers/xor_gate.v
@@ -1,2 +1,12 @@
-module xor (input a, b, output y)
+module xor (input a, b, output y):
+  xor_gate xor_gate(
+    .a(a),
+    .b(b),
+    .y(y),
+):
+  initial begin
+    $dumpfile("xor.vcd");
+    $dumpvars;
+  end
+endmodule
   
